a3669dbf8936326169cb93ae019de63fa3e08f71
srutiranjan-95 <146977542+srutiranjan-95@users.noreply.github.com>
2024-10-22 03:30:50 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "bubu"
+
+// console.log (typeof score);
+// console.log(typeof(score));
+
+let valueInnumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInnumber);
+
+// "33" => 33
+// "33abc" => NaN
+// "true" => 1; false => 0 
+
+let isLoggedIn = "bubu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
