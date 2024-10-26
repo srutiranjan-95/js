eeb483bfd0177c60c39a4cbe9c3a013b1e4f9c7f
srutiranjan-95 <146977542+srutiranjan-95@users.noreply.github.com>
2024-10-26 13:31:05 +0000

    change in location file

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+// Primitive
+
+// 7 types : Stringf, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log( id === anotherId);
+
+const bigNumber = 12345678456789n
+
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions
